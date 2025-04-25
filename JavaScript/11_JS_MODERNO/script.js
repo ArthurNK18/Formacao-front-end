d3482d71e1934c34e2a6d8b10adf6af308f105ce
ArthurNK18 <arthur123_NK@outlook.com>
2025-04-25 17:57:08 -0300

    Update / JavaScript Moderno

--- JavaScript/11_JS_MODERNO/script.js
+++ JavaScript/11_JS_MODERNO/script.js
@@ -53,15 +53,148 @@ console.log(greet("John"))
 
 console.log(greet())
 
-const user = {
-    name: "Arthur",
-    sayUserName() {
-        setTimeout(function () {
-            console.log(this);
-            console.log(`Username: ${this.name}`);
-        }, 1000)
-    },
+// const user = {
+//     name: "Arthur",
+//     sayUserName() {
+//         setTimeout(function () {
+//             console.log(this);
+//             console.log(`Username: ${this.name}`);
+//         }, 1000)
+//     },
+//     sayUserNameArrow () {
+//         setTimeout(() => {
+//             console.log(this);
+//             console.log(`Username: ${this.name}`);
+//         }, 1000)
+//     }
+// }
+
+// // user.sayUserName() 
+// user.sayUserNameArrow()
+
+// filter
+const arr = [1, 2, 3, 4, 5, 6]
+
+const highNumbers = arr.filter ((n) => {
+    if(n >= 3) {
+        return n
+    }
+})
+
+console.log(highNumbers) // [3, 4, 5, 6]    
+
+
+const users = [
+    {name: "Arthur", avaiable: true},
+    {name: "Hidan", avaiable: false},
+    {name: "Naruto", avaiable: true},
+    {name: "Howl", avaiable: false},
+    {name: "Shikamaru", avaiable: true},
+]
+
+const availableUsers = users.filter((user) => user.avaiable) 
+
+console.log(availableUsers)
+
+// Map
+const products = [
+    {name: "Shirt", price: 10.99, category: "Roupas"},
+    {name: "Chaleira Eletrica", price: 150, category: "Eletro"},
+    {name: "Fogão", price: 499, category: "Eletro"},
+    {name: "Calça Jeans", price: 30.99, category: "Roupas"},
+]
+
+products.map ((product) => {
+    if(product.category === "Roupas") {
+        product.onSale = true
+    }
+})
+
+console.log(products)
+
+// Templet literals
+const userName = "Arthur"
+const age = 19
+
+console.log(`O nome do usuário é: ${userName} e ele tem ${age} anos.`)
+
+// Destructuring
+const fruits = ["Banana", "Maçã", "Laranja"]
+
+const [f1, f2, f3] = fruits
+
+console.log(f1, f2, f3) // Banana, Maçã, Laranja
+
+const productDetails = {
+    name: "Mouse",
+    price: 39.99,
+    category: "Periféricos",
+    color: "Preto", 
 }
 
-user.sayUserName()
+const {name: productName, price, category, color} = productDetails;
 
+console.log(`O nome do produto é: ${productName}, ele custa R$${price}, e da cor: ${color}.`);
+
+// Spred Oprator
+const a1 = [1, 2, 3]
+const a2 = [4, 5, 6]
+
+const a3 = [...a1, ...a2]
+
+console.log(a3) // [1, 2, 3, 4, 5, 6]
+
+const a4 = [0, ...a3, 7]
+
+console.log(a4) // [0, 1, 2, 3, 4, 5, 6, 7]
+
+const carName = {name: "Gol"}
+const carBrand = {brand: "Volkswagen"}
+const otherInfos = {KM: 10000, price: 50000};
+
+const car = {...carName, ...carBrand, ...otherInfos};
+
+console.log(car);
+
+// Classes
+class product {
+    constructor(name, price) {
+        this.name = name
+        this.price = price
+    }
+
+    productWhithDiscount(discaunt) {
+        return this.price * ((100 - discaunt) / 100)
+
+    }
+}
+
+const shirt = new product("Camisa", 20)
+
+console.log(shirt)
+
+console.log(shirt.productWhithDiscount(10))
+console.log(shirt.productWhithDiscount(15))
+
+// Herança
+class productWithAtributtes extends product {
+    constructor(name, price, colors) {
+        super(name, price)
+        this.colors = colors
+    }
+
+    showColors() {
+        console.log(`As cores são:`)
+        this.colors.forEach((color) => {
+            console.log(color)
+        })
+    }
+}
+
+const hat = new productWithAtributtes("Chapéu", 29.99, ["Preto", "Branco", "Vermelho"])
+
+console.log(hat)
+
+console.log(hat.name)
+
+hat.showColors()
